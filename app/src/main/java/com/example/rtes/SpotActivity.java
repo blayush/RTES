4c26b8f3412199537bf2f996355f5f4dc0d1a806
blayush <ayushmishra42@gmail.com>
2022-07-18 22:27:42 +0530

    intial commit

---
+++ app/src/main/java/com/example/rtes/SpotActivity.java
@@ -0,0 +1,14 @@
+package com.example.rtes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SpotActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spot);
+    }
+}
