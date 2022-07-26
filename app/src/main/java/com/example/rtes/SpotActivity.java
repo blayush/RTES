9ed70c178101158224e76a1846f5112e36013bb9
blayush <ayushmishra42@gmail.com>
2022-07-26 09:22:37 +0530

    Spot Activity UI Animation Added

--- app/src/main/java/com/example/rtes/SpotActivity.java
+++ app/src/main/java/com/example/rtes/SpotActivity.java
@@ -1,18 +1,38 @@
 package com.example.rtes;
 
-import static com.example.rtes.R.color.colorPurple;
+import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.graphics.drawable.ColorDrawable;
-import android.os.Bundle;
+import com.example.rtes.databinding.ActivitySpotBinding;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SpotActivity extends AppCompatActivity {
 
+    private DatabaseReference rootRef;
+    private Boolean flag;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_spot);
-        getWindow().setStatusBarColor(getResources().getColor(colorPurple));
+        com.example.rtes.databinding.ActivitySpotBinding binding = ActivitySpotBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        rootRef= FirebaseDatabase.getInstance().getReference();
+        flag = false;
+
+        binding.spotBtn.setOnClickListener(view1 -> {
+
+            if(!flag) {
+                binding.popLayout.animate().translationYBy(-1520).rotationBy(-360).setDuration(500);
+                flag=true;
+            }
+
+
+        });
+
     }
 }
